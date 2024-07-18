f569768663badcb475a3eb3a4bc0e13ee0e0215f
dinhnguyet1110 <dinhnguyet11102004@gmail.com>
2024-07-18 14:15:13 +0700

    update

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -48,55 +48,12 @@ class ContentController extends Controller
     // Lấy tất cả các bài viết, video, và hình ảnh được bình luận bởi một người dùng cụ thể
     public function getUserCommentedContent($userId)
     {
-        $comments = Comment::where('user_id', $userId)->get();
-    
-        $articles = $comments->filter(function ($comment) {
-            return $comment->commentable_type == Article::class;
-        })->pluck('commentable');
-    
-        $videos = $comments->filter(function ($comment) {
-            return $comment->commentable_type == Video::class;
-        })->pluck('commentable');
-    
-        $images = $comments->filter(function ($comment) {
-            return $comment->commentable_type == Image::class;
-        })->pluck('commentable');
-    
-        dd([
-            'articles' => $articles,
-            'videos' => $videos,
-            'images' => $images,
-        ]);
+       
     }
     
     // Lấy danh sách các bài viết, video, và hình ảnh có đánh giá trung bình cao nhất
     public function getTopRatedContent()
     {
-        $topRatedArticles = Article::with(['ratings' => function($query) {
-            $query->select(DB::raw('rateable_id, AVG(rating) as average_rating'))
-                  ->groupBy('rateable_id')
-                  ->orderBy('average_rating', 'desc')
-                  ->take(5);
-        }])->get();
-    
-        $topRatedVideos = Video::with(['ratings' => function($query) {
-            $query->select(DB::raw('rateable_id, AVG(rating) as average_rating'))
-                  ->groupBy('rateable_id')
-                  ->orderBy('average_rating', 'desc')
-                  ->take(5);
-        }])->get();
-    
-        $topRatedImages = Image::with(['ratings' => function($query) {
-            $query->select(DB::raw('rateable_id, AVG(rating) as average_rating'))
-                  ->groupBy('rateable_id')
-                  ->orderBy('average_rating', 'desc')
-                  ->take(5);
-        }])->get();
-    
-        dd([
-            'top_rated_articles' => $topRatedArticles,
-            'top_rated_videos' => $topRatedVideos,
-            'top_rated_images' => $topRatedImages,
-        ]);
+      
     }
 }
